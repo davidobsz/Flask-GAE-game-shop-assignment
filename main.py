38c6bfbb11b894f9a3ec0436d105f5cda602ab18
davidobsz <davidobszanski@icloud.com>
2022-11-11 16:13:26 +0000

    added registration using MySQL database

--- main.py
+++ main.py
@@ -1,107 +1,158 @@
-import logging
-from flask import Flask, render_template,request
-import flask
 import json
-from flask import request, jsonify
-from pymongo import MongoClient
-from bson.json_util import dumps 
-import requests
+import logging
 
+import requests
+from bson.json_util import dumps
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask import jsonify
+from pymongo import MongoClient
+import mysql.connector
+from mysql.connector import Error, errorcode
 
 app = Flask(__name__)
+
+mydb = mysql.connector.connect(host='localhost',
+                               database='userlogin',
+                               user='root',
+                               password='1234')
+mycursor = mydb.cursor()
+
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html')
+    return render_template('home.html')
+
+
 @app.route('/about')
 def about():
- return render_template('about.html')
+    return render_template('about.html')
+
+
 @app.route('/register')
 def form():
- return render_template('register.html')
+
+    mycursor.execute("SELECT * FROM accounts ")
+    for x in mycursor:
+        print(x)
+    return render_template('register.html')
+
+
 # [END form]
 # [START submitted]
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
- name = request.form['name']
- email = request.form['email']
- site = request.form['site_url']
- comments = request.form['comments']
- # [END submitted]
- # [START render_template]
- return render_template(
-  'submitted_form.html',
-  name=name,
-  email=email,
-  site=site,
-  comments=comments)
- # [END render_template]
-#-------------------------------------------
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+    # [END render_template]
+
+
+# -------------------------------------------
 cluster = MongoClient("mongodb+srv://d:d@cluster0.ccyermz.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["Pythontest"]
 collection = db["Students"]
 
 
-
 def get_mongodb_items():
- # Search data from Mongodb
- myCursor = None
- # create queries
- title_query = {"Unit title": {"$eq": "IoT Unit"}}
- author_query = {"Unit leader": {"$eq": "Xin"}}
- dateCreated_query = {"dateCreated": {"$eq": 2021}}
- myCursor = collection.find({"$and": [title_query, author_query, dateCreated_query]})
- list_cur = list(myCursor)
- print(list_cur)
- json_data = dumps(list_cur)
- return json_data 
+    # Search data from Mongodb
+    myCursor = None
+    # create queries
+    title_query = {"Unit title": {"$eq": "IoT Unit"}}
+    author_query = {"Unit leader": {"$eq": "Xin"}}
+    dateCreated_query = {"dateCreated": {"$eq": 2021}}
+    myCursor = collection.find({"$and": [title_query, author_query, dateCreated_query]})
+    list_cur = list(myCursor)
+    print(list_cur)
+    json_data = dumps(list_cur)
+    return json_data
 
 
 def store_mongodb(Unittitle, Unitleader, content, dateCreated, thumbnail):
- # Write to MongoDB
- json_data = {"Unit title": Unittitle, "Unit leader": Unitleader, "dateCreated":dateCreated, "thumbnail": thumbnail, "content": content}
- collection.insert_one(json_data) 
+    # Write to MongoDB
+    json_data = {"Unit title": Unittitle, "Unit leader": Unitleader, "dateCreated": dateCreated, "thumbnail": thumbnail,
+                 "content": content}
+    collection.insert_one(json_data)
+
 
 @app.route('/unit')
 def Post_Mongo():
- store_mongodb('IoT Unit', 'Xin', 'Welcome to IoT Unit', 2021, ',')
- return "done"
+    store_mongodb('IoT Unit', 'Xin', 'Welcome to IoT Unit', 2021, ',')
+    return "done"
 
 
 @app.route('/display', methods=['GET'])
 def display():
- jResponse=get_mongodb_items()
- data=json.loads(jResponse)
- return jsonify(data)
+    jResponse = get_mongodb_items()
+    data = json.loads(jResponse)
+    return jsonify(data)
 
 
 # cloud function <- test TEST TEST TEST
 @app.route("/shop", methods=["GET"])
 def hello():
- url= "https://europe-west2-river-psyche-366910.cloudfunctions.net/shop-items"
- response=requests.get(url)
- responseString = response.content
- responseString = str(responseString)
- responseString = responseString[3:-2]
+    url = "https://europe-west2-river-psyche-366910.cloudfunctions.net/shop-items"
+    response = requests.get(url)
+    responseString = response.content
+    responseString = str(responseString)
+    responseString = responseString[3:-2]
 
- responseString2 = "{\"items\":["+responseString+"]}"
- data = json.loads(responseString2)
- print(data)
- print(data["items"][0]["name"])
+    responseString2 = "{\"items\":[" + responseString + "]}"
+    data = json.loads(responseString2)
+    print(data)
+    print(data["items"][0]["name"])
 
- return render_template("shop.html", response=data["items"])
+    return render_template("shop.html", response=data["items"])
 
-#-------------------------------------------
 
+@app.route("/login", methods=["GET", "POST"])
+def firebase():
+    error = ""
+    try:
+        if request.method == "POST":
+            username = request.form.get("name")
+            password = request.form.get("password")
+            email = request.form.get("email")
+            print(username, password, email)
+            sql = "INSERT INTO accounts VALUES('{}', {}, '{}')".format(username, password,email)
+            print(sql)
+            mycursor.execute(sql)
+            mydb.commit()
+            print(mycursor.rowcount, "record inserted.")
+    except mysql.connector.Error as e:
+        #print(e)
+        #print(e.errno)
+        #print(e.msg)
+        if(e.errno == 1062):
+            error = "USERNAME OR EMAIL ALREADY USED"
+
+
+
+    return render_template("login.html", error=error)
+
+
+# -------------------------------------------
 
 @app.errorhandler(500)
 def server_error(e):
- # Log the error and stacktrace.
- logging.exception('An error occurred during a request.')
- return 'An internal error occurred.', 500
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
+
 @app.errorhandler(404)
 def page_not_found(error):
- return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
- # Only run for local development.
- app.run(host='127.0.0.1', port=8080, debug=True) 
+    # Only run for local development.
+    app.run(host='127.0.0.1', port=8080, debug=True)
